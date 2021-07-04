d579a5a74f10c6b4ab16a27d53e61d4e82e2727d
Sergio Cabral <git@sergiocabral.com>
2021-07-04 01:21:37 -0300

    global nãoprecisa ser var

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const fs = require('fs');
 const tmi = require('tmi.js');
 const OBSWebSocket = require('obs-websocket-js');
 
-var global = {
+const global = {
     /**
      * @type {OBSWebSocket}
      */
