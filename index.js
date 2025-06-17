60aab98a7d93aed09d56ba1f0154ddf336254fea
Sergio Cabral <git@sergiocabral.com>
2025-06-16 23:26:25 -0300

    chore: comma removed

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import dotenv from 'dotenv';
-import { exec } from 'child_process';
+import dotenv from 'dotenv'
+import { exec } from 'child_process'
 
 class App {
   initialize() {
-    console.debug('Initializing...');
-    dotenv.config();
+    console.debug('Initializing...')
+    dotenv.config()
   }
 
   notify(file = 'notification.wav') {
-    const command = `ffplay -nodisp -autoexit -hide_banner -loglevel quiet "${file}"`;
+    const command = `ffplay -nodisp -autoexit -hide_banner -loglevel quiet "${file}"`
     exec(command, error => {
       if (error) {
         console.error(`Media player not found: ${String(error ?? '').replace(/\n|\r/g, ' ').replace(/\s+/g, ' ')}`)
       }
-    });
+    })
   }
 
   async run() {
     try {
-      this.initialize();
-      await this.notify();
-      console.log('Running.');
+      this.initialize()
+      await this.notify()
+      console.log('Running.')
     } catch (error) {
-      console.error('Erro ao executar:', error);
+      console.error('Erro ao executar:', error)
     }
   }
 }
 
-new App().run();
+new App().run()
